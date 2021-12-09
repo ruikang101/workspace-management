6da0d9f8610fa73526b965ecfd3975894366525d
Zhifei Cheng <zc5dp@uw.edu>
2021-12-09 03:37:43 -0800

    Add JWT authorization in requests expect for login and weather info

--- backend/src/main/java/tcss556/configurations/AppConfig.java
+++ backend/src/main/java/tcss556/configurations/AppConfig.java
@@ -4,9 +4,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.validator.routines.InetAddressValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import tcss556.filters.JWTFilter;
+
+import javax.annotation.Resource;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
+
+    @Resource
+    private JWTFilter filter;
 
     @Bean
     public ObjectMapper objectMapper(){
@@ -16,5 +24,10 @@ public class AppConfig {
     @Bean
     public InetAddressValidator validator(){
         return InetAddressValidator.getInstance();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(filter).addPathPatterns("/rooms/*", "/users/*", "/meetings/*");
     }
 }
--- backend/src/main/java/tcss556/services/controllers/UserLoginEndpoint.java
+++ backend/src/main/java/tcss556/services/controllers/UserLoginEndpoint.java
@@ -1,19 +1,31 @@
 package tcss556.services.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 import tcss556.services.models.wsdl.LoginRequest;
 import tcss556.services.models.wsdl.LoginResponse;
+import tcss556.services.models.wsdl.LoginStatus;
+import tcss556.utils.auth.JWTUtil;
+
+import javax.annotation.Resource;
 
 @Endpoint
 public class UserLoginEndpoint {
     private static final String NAMESPACE_URI = "http://tcss556/services/models/wsdl";
 
+    @Resource
+    private ObjectMapper mapper;
+
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "LoginRequest")
     @ResponsePayload
-    public LoginResponse tryLogin(@RequestPayload LoginRequest request) {
-        return LoginResponse.SUCCEED;
+    public LoginResponse tryLogin(@RequestPayload LoginRequest request) throws JsonProcessingException {
+        LoginResponse response = new LoginResponse();
+        response.setStatus(LoginStatus.SUCCEED);
+        response.setToken(JWTUtil.createJWT(mapper.writeValueAsString(request), 36000L));
+        return response;
     }
 }
